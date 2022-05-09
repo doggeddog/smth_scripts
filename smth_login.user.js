7441c2b159633bb3572b2ba77fde11acbf0b836c
doggeddog <devoteever@gmail.com>
2022-05-09 20:08:24 +0800

    support for m.mysmth.net

--- smth_login.user.js
+++ smth_login.user.js
@@ -2,11 +2,11 @@
 // @name         水木保持登录
 // @namespace    https://github.com/doggeddog
 // @homepage     https://github.com/doggeddog/smth_scripts
-// @version      1.0
+// @version      1.1
 // @description  水木社区经常掉线, 这个脚本通过自动刷新保持登陆状态.
 // @author       doggeddog
 // @match        *://www.newsmth.net/*
-// @match        *://www.mysmth.net/*
+// @match        *://*.mysmth.net/*
 // @run-at       document-end
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -16,14 +16,15 @@
 function refresh() {
     var refreshTime = GM_getValue('refreshTime');
     var now = Date.now();
-    console.log("refresh", refreshTime, now);
     // 5分钟刷新状态一次
     if(now > refreshTime + 300000){
-        SESSION.update(true);
         GM_setValue('refreshTime', now);
-        console.log("session updated");
+        if (window.location.hostname == 'www.mysmth.net') {
+            SESSION.update(true);
+        } else if (window.location.hostname == 'm.mysmth.net') {
+            location.reload();
+        }
     }
 }
-
 GM_setValue('refreshTime', Date.now());
 setInterval(refresh, 60000);
