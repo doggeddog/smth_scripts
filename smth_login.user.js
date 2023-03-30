ac40955db6d31be49704dcd77ff34209132bc0bc
doggeddog <devoteever@gmail.com>
2023-03-30 16:13:59 +0800

    添加newsmth域名

--- smth_login.user.js
+++ smth_login.user.js
@@ -2,10 +2,10 @@
 // @name         水木保持登录
 // @namespace    https://github.com/doggeddog
 // @homepage     https://github.com/doggeddog/smth_scripts
-// @version      1.2
+// @version      1.3
 // @description  水木社区经常掉线, 这个脚本通过自动刷新保持登陆状态.
 // @author       doggeddog
-// @match        *://www.newsmth.net/*
+// @match        *://*.newsmth.net/*
 // @match        *://*.mysmth.net/*
 // @run-at       document-end
 // @grant        GM_setValue
@@ -20,9 +20,9 @@ function refresh() {
     // 5分钟刷新状态一次
     if(now > refreshTime + 300000){
         GM_setValue('refreshTime', now);
-        if (window.location.hostname == 'www.mysmth.net') {
+        if (window.location.hostname == 'www.mysmth.net' || window.location.hostname == 'www.newsmth.net') {
             SESSION.update(true);
-        } else if (window.location.hostname == 'm.mysmth.net') {
+        } else if (window.location.hostname == 'm.mysmth.net' || window.location.hostname == 'm.newsmth.net') {
             GM_xmlhttpRequest( {
                 method: "GET",
                 url: location.href,
