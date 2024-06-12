f4550b1753ab342cf6cc4df03b379ddb15bdf9a5
doggeddog <devoteever@gmail.com>
2024-06-12 17:33:42 +0800

    支持翻页快捷键

--- smth_login.user.js
+++ smth_login.user.js
@@ -2,7 +2,7 @@
 // @name         水木保持登录
 // @namespace    https://github.com/doggeddog
 // @homepage     https://github.com/doggeddog/smth_scripts
-// @version      1.4
+// @version      2.0
 // @description  水木社区经常掉线, 这个脚本通过自动刷新保持登陆状态.
 // @author       doggeddog
 // @match        *://*.newsmth.net/*
@@ -29,5 +29,31 @@ function refresh() {
         });
     }
 }
+
+function left() {
+    var x = document.querySelector("#body > div.t-pre > div.page > ul > li:nth-child(2) > ol > li:first-child > a");
+    x.click();
+}
+
+function right() {
+    var x = document.querySelector("#body > div.t-pre > div.page > ul > li:nth-child(2) > ol > li:last-child > a");
+    x.click();
+}
+
+function keyMapping() {
+    document.addEventListener("keydown", (event) => {
+        switch (event.key) {
+            case "ArrowLeft":
+                left();
+                break;
+            case "ArrowRight":
+                right();
+                break;
+        }
+    });
+}
+
 GM_setValue('refreshTime', Date.now());
 setInterval(refresh, 30000);
+keyMapping();
+
